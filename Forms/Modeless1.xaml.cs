05d6f1b68bcc6c4579384d307b84f7a8ab07bf81
Adrian Esdaile <adrian.esdaile@jpw.com.au>
2025-10-01 15:48:44 +1000

    Add project files.

---
+++ Forms/Modeless1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RAA2_Level_Checker.Forms
+{
+    /// <summary>
+    /// Interaction logic for Modeless1.xaml
+    /// </summary>
+    public partial class Modeless1 : Window
+    {
+        public Modeless1()
+        {
+            InitializeComponent();
+        }
+    }
+}
